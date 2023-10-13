b9ecdfb3c746a9b7c9f98e2a3f9a6a9ad3ba8928
kal-elf2 <kkingpharmd@hotmail.com>
2023-10-12 22:48:49 -0500

    unequip works, equip armor broken

--- resources/backpack.py
+++ resources/backpack.py
@@ -76,18 +76,83 @@ class BackpackView(discord.ui.View):
         self.update_or_add_item_type_select("unequip")
         await interaction.response.edit_message(content="Choose an item type to unequip:", view=self)
 
+
 class ItemTypeSelect(discord.ui.Select):
     def __init__(self, action_type, options):
         self.action_type = action_type
         super().__init__(placeholder=f"Choose an item type to {action_type}", options=options, min_values=1, max_values=1)
 
     async def callback(self, interaction: discord.Interaction):
+
+        def get_armor_type_by_name(name: str, inventory):
+            """Utility function to get armor type based on the name from the equipped armor."""
+            for armor_key, armor_data in inventory.equipped_armor.items():
+                if armor_data and armor_data.name == name:
+                    return armor_key  # Return the key, which corresponds to the armor type.
+            return None
+
         # Handle the selected item type and perform either equip or unequip action
         selected_value = self.values[0]
 
         # Retrieve the player's inventory
         view: BackpackView = self.view
         inventory = view.inventory
+
+        # Handle Unequipping for single-slot categories
+        if self.action_type == "unequip" and selected_value in ["Weapon", "Shield", "Charm"]:
+            category_singular = selected_value.lower()
+            equipped_item_key = f"equipped_{category_singular}"
+            equipped_item = getattr(inventory, equipped_item_key, None)
+
+            if equipped_item:
+                # Check if this item already exists in inventory
+                existing_item = next((item for item in getattr(inventory, category_singular + "s", []) if
+                                      item.name == equipped_item.name), None)
+                if existing_item:
+                    existing_item.stack += 1
+                else:
+                    getattr(inventory, category_singular + "s").append(copy.deepcopy(equipped_item))
+
+                # Unequip the item
+                setattr(inventory, equipped_item_key, None)
+
+                update_and_save_player_data(interaction, inventory, view.player_data)
+                embed = create_item_embed(self.action_type, equipped_item)
+                await interaction.response.send_message(embed=embed, ephemeral=True)
+                view.refresh_view()
+                return
+            else:
+                embed = create_item_embed(self.action_type, selected_value)
+                await interaction.response.send_message(embed=embed, ephemeral=True)
+                return
+
+        # Handle Unequipping for Armor type
+        if self.action_type == "unequip" and selected_value == "Armor":
+            # Gather currently equipped armor pieces
+            equipped_armors = [Armor.from_dict(armor_data) if isinstance(armor_data, dict) else armor_data
+                               for armor_data in inventory.equipped_armor.values() if armor_data is not None]
+
+            if not equipped_armors:
+                embed = create_item_embed(self.action_type, selected_value)
+                await interaction.response.send_message(embed=embed, ephemeral=True)
+                return
+
+            # Clear existing options
+            self.options.clear()
+
+            # Populate the dropdown with currently equipped armor pieces
+            for armor in equipped_armors:
+                option = discord.SelectOption(label=armor.name, value=armor.name,
+                                              emoji=getattr(armor, "emoji", None))
+                self.options.append(option)
+
+            # Reset the placeholder to reflect the change
+            self.placeholder = f"Choose a specific {selected_value} to {self.action_type}"
+
+            # Update the interaction message to show the updated options
+            await interaction.response.edit_message(
+                content=f"Choose a specific {selected_value} to {self.action_type}:", view=view)
+            return
 
         # If the selected value corresponds to an item type, show the items
         if selected_value in ["Weapon", "Armor", "Shield", "Charm"]:
@@ -118,9 +183,55 @@ class ItemTypeSelect(discord.ui.Select):
             await interaction.response.edit_message(content=f"Choose a specific {selected_value} to {self.action_type}:", view=view)
             return
 
-
-        # If execution reaches here, it means an actual item was selected
         selected_item_name = self.values[0]
+        print(f"Selected armor name: {selected_item_name}")
+
+        # Find the selected item based on its name
+        selected_item = None
+        for category in ["weapons", "shields", "charms"]:
+            selected_item = next((item for item in getattr(inventory, category, []) if item.name == selected_item_name),
+                                 None)
+            if selected_item:
+                break
+
+        # Special handling for the "armors" category
+        if not selected_item:
+            for armor_type, armor in inventory.equipped_armor.items():
+                if armor and armor.name == selected_item_name:
+                    selected_item = armor
+                    break
+
+        # If no item is found, send an error message and return
+        if not selected_item:
+            await interaction.response.send_message(f"No item found with name: {selected_item_name}", ephemeral=True)
+            return
+
+        # If unequip action and the item is an armor
+        if self.action_type == "unequip" and isinstance(selected_item, Armor):
+
+            # Get the type of the armor
+            armor_type = get_armor_type_by_name(selected_item_name, inventory)
+            print(f"Identified armor type: {armor_type}")
+
+            # Check if this armor is currently equipped
+            if inventory.equipped_armor[armor_type] and inventory.equipped_armor[armor_type].name == selected_item_name:
+                print(f"Current equipped armors before update: {inventory.equipped_armor}")
+                # Put the armor back to the inventory
+                existing_armor = next((armor for armor in inventory.armors if armor.name == selected_item_name), None)
+                if existing_armor:
+                    existing_armor.stack += 1
+                else:
+                    inventory.armors.append(copy.deepcopy(inventory.equipped_armor[armor_type]))
+
+                # Unequip the armor
+                inventory.equipped_armor[armor_type] = None
+
+                # Save, send a response and refresh
+                update_and_save_player_data(interaction, inventory, view.player_data)
+                embed = create_item_embed(self.action_type, selected_item)
+                await interaction.response.send_message(embed=embed, ephemeral=True)
+                view.refresh_view()
+                return
 
         for category in ["weapons", "armors", "shields", "charms"]:
             selected_item = next((item for item in getattr(inventory, category, []) if item.name == selected_item_name),
@@ -178,23 +289,6 @@ class ItemTypeSelect(discord.ui.Select):
                     else:
                         existing_item.stack -= 1
 
-            # Unequip logic
-            elif self.action_type == "unequip":
-                if isinstance(selected_item, Armor):
-                    if inventory.equipped_armor[selected_item.armor_type] == selected_item:
-                        if existing_item:
-                            existing_item.stack += 1
-                        else:
-                            inventory.armors.append(selected_item)
-                        inventory.equipped_armor[selected_item.armor_type] = None
-
-                else:
-                    if getattr(inventory, equipped_item_key) == selected_item:
-                        if existing_item:
-                            existing_item.stack += 1
-                        else:
-                            getattr(inventory, category).append(selected_item)
-                        setattr(inventory, equipped_item_key, None)
 
             update_and_save_player_data(interaction, inventory, view.player_data)
             embed = create_item_embed(self.action_type, selected_item)
